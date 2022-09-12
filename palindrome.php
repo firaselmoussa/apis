479877c9fa4ab082125546e81379f3b69492ae48
Phoenix-Firas <106271549+Phoenix-Firas@users.noreply.github.com>
2022-09-13 00:22:10 +0300

    implemented palindrome.php api

---
+++ palindrome.php
@@ -0,0 +1,18 @@
+<?php
+
+$palindrome = "false";
+
+$string =  $_GET["string"];
+$string_array = str_split($string);
+
+$reversed_string_array = array_reverse($string_array);
+
+if($string_array == $reversed_string_array){
+    $palindrome = "true";
+}else{
+    $palindrome = "false";
+};
+
+echo json_encode($palindrome);
+
+?>
